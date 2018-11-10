94bc3e8facdb8ff69d6e0e1f03f920269602bf1b
kimkundad <kim.kundad@gmail.com>
2018-11-11 06:52:39 +0700

    golf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      
         //
        \URL::forceScheme('https');
         Schema::defaultStringLength(191);
