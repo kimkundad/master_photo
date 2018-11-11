8e0dfa59c46d8acb3f2a5018ee56497962b5a19e
kimkundad <kim.kundad@gmail.com>
2018-11-12 05:53:06 +0700

    golf

--- resources/views/photo_edit.blade.php
+++ resources/views/photo_edit.blade.php
@@ -4,11 +4,129 @@
 Photo print
 @stop
 
-@section('stylesheet')
+
 
 <link rel="stylesheet" href="{{url('master/assets/css/dropzone.css')}}">
+<link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+<link href="{{url('master/assets/css/base.css')}}" rel="stylesheet">
+<link href="{{url('css/style.css')}}" rel="stylesheet">
+<link href="{{url('master/assets/css/menu.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{url('master/assets/css/icons.css')}}" >
+<link href="{{url('master/assets/css/skins/square/grey.css')}}" rel="stylesheet">
+<link rel="stylesheet" href="{{url('master/assets/css/slider-pro.min.css')}}" />
+<link rel="stylesheet" type="text/css" href="{{url('master/assets/slick/slick-theme.css')}}"/>
+<link rel="stylesheet" type="text/css" href="{{url('master/assets/slick/slick.css')}}"/>
 
-@stop('stylesheet')
+<style>
+  body,
+  html,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  span,
+  strong {
+    font-family: 'Kanit', sans-serif;
+  }
+  p {
+    font-size: 14px;
+  }
+  body {
+
+    font-size: 13px;
+  }
+
+  .slider-pro {
+    margin-top: 120px;
+  }
+
+  ul#top_links li {
+    font-size: 13px;
+  }
+  a.btn_full_outline_golf {
+    color: #333;
+    background: 0 0;
+    border: 2px solid #333;
+    padding: 10px 20px;
+    transition: all .3s;
+    -webkit-border-radius: 3px;
+    -moz-border-radius: 3px;
+    border-radius: 3px;
+}
+a.btn_full_outline_golf:hover {
+
+    color: #fff;
+    background:#333
+}
+#social_footer ul li img {
+    height: 30px;
+}
+a.button_drop, .button_drop {
+  margin-top:1px;
+    font-family: inherit;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 11px;
+    text-align: center;
+    text-transform: uppercase;
+    width: 49%;
+    color: rgb(51, 51, 51) !important;
+    font-weight: 700 !important;
+    border-width: initial;
+    border-style: none;
+    border-color: initial;
+    border-image: initial;
+    background: rgb(255, 212, 48);
+    outline: none;
+    transition: all 0.3s ease 0s;
+    border-radius: 3px;
+    padding: 5px 15px !important;
+}
+a.button_drop:hover {
+    margin-top:1px;
+    font-family: inherit;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 11px;
+    text-align: center;
+    text-transform: uppercase;
+    width: 49%;
+    color: rgb(51, 51, 51) !important;
+    font-weight: 700 !important;
+    border-width: initial;
+    border-style: none;
+    border-color: initial;
+    border-image: initial;
+    background: rgb(255, 212, 48);
+    outline: none;
+    transition: all 0.3s ease 0s;
+    border-radius: 3px;
+    padding: 5px 15px !important;
+}
+a.button_drop.outline, .button_drop.outline {
+    padding: 5px 15px !important;
+    color: rgb(51, 51, 51);
+    background: rgb(255, 212, 48);
+    border-width: 1px;
+    border-style: solid;
+    background: rgb(255, 212, 48);
+    border-image: initial;
+}
+a.button_drop.outline:hover, .button_drop.outline:hover {
+    padding: 5px 15px !important;
+    color: rgb(51, 51, 51);
+    background: rgb(255, 212, 48);
+    border-width: 1px;
+    border-style: solid;
+    background: rgb(255, 212, 48);
+    border-image: initial;
+}
+</style>
+
+
 @section('content')
 
 
@@ -66,26 +184,7 @@ Photo print
 }
    }
 
-   @media (max-width: 767px){
-     .table.cart-list, .table.cart-list tbody, .table.cart-list td, .table.cart-list th, .table.cart-list thead, .table.cart-list tr {
-       display: block;
-       border: 0;
-   }
-   .table.cart-list tr {
-       border: 1px solid #ccc;
-   }
-   .table.cart-list td {
-       border: none;
-       border-bottom: 1px solid #ddd;
-       position: relative;
-       padding-left: 50%;
-   }
-   .table.cart-list td:nth-of-type(1):before {
-       content: "Item";
-       font-weight: 700;
-       color: #111;
-   }
-   }
+
 
    .thumb_cart1 {
     border: 1px solid #ddd;
