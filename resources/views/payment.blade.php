dbea745e4b436dde9e45ccc13248725273c16625
kimkundad <kim.kundad@gmail.com>
2019-05-07 16:28:09 +0700

    golf

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -197,7 +197,7 @@ Payment | MASTER PHOTO NETWORK
         $payment_description  = '2 days 1 night hotel room';
         $order_id  = $order->code_gen;
         $currency = "764";
-        $amount  = '000000008600';
+        $amount  = '000000008650';
 
         //Request information
         $version = "8.5";
