ee3ff42fd4d6f96c55b28be9e91a1787347bbf72
kimkundad <kim.kundad@gmail.com>
2019-05-23 15:33:17 +0700

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        
         \URL::forceScheme('https');
         Schema::defaultStringLength(191);
 
